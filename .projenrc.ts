efcae731d9bf0240b6f1170887f62558e4c56247
Alex Zero <me@alex0.net>
2022-12-14 06:45:08 +0000

    fix: npmAccess public

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
 import { typescript } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
 
 const project = new typescript.TypeScriptProject({
   authorName: 'Arctarus Limited',
@@ -13,6 +14,7 @@ const project = new typescript.TypeScriptProject({
 
   projenrcTs: true,
   releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
 
   // dependencies
   deps: [
